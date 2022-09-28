817c3a2cd93c63d63af368ff15b0d473875e3ec1
silence_zhpf <silence_zhpf@aliyun.com>
2022-09-28 23:40:21 +0800

    feat: ✨ 添加区间关系取反操作

--- src/interval/mod.rs
+++ src/interval/mod.rs
@@ -1,3 +1,37 @@
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn left_reverse_test() {
+        let relation = Relation::Left.reverse();
+        assert_eq!(relation, Relation::Right);
+    }
+    #[test]
+    fn right_reverse_test() {
+        let relation = Relation::Right.reverse();
+        assert_eq!(relation, Relation::Left);
+    }
+    #[test]
+    fn include_reverse_test() {
+        let relation = Relation::Include.reverse();
+        assert_eq!(relation, Relation::Included);
+    }
+    #[test]
+    fn included_reverse_test() {
+        let relation = Relation::Included.reverse();
+        assert_eq!(relation, Relation::Include);
+    }
+    #[test]
+    fn left_intersection_reverse() {
+        let relation = Relation::LeftIntersection.reverse();
+        assert_eq!(relation, Relation::RightIntersection);
+    }
+    #[test]
+    fn right_intersection_reverse() {
+        let relation = Relation::RightIntersection.reverse();
+        assert_eq!(relation, Relation::LeftIntersection);
+    }
+}
 /**
 区间之间的关系类型：
 
@@ -23,6 +57,28 @@ pub enum Relation {
     /// Included 这个区间被另一个区间所包含
     Included,
 }
+impl Relation {
+    /**
+     * 获取区间关系的取反
+     *
+     * - `Relation::Include`<-> `Relation::Included` 两个区间互相是包含和被包含的关系
+     * - `Relation::Left` <-> `Relation::Right` 两个区间互相是左右关系
+     * - `Relation::LeftIntersection` <-> `Relation::RightIntersection` 两个区间互相是左右部分交集的关系
+     */
+    pub fn reverse(&self) -> Relation {
+        match self {
+            Relation::Left => Relation::Right,
+            Relation::Right => Relation::Left,
+            Relation::Included => Relation::Include,
+            Relation::Include => Relation::Included,
+            Relation::LeftIntersection => Relation::RightIntersection,
+            Relation::RightIntersection => Relation::LeftIntersection,
+        }
+    }
+}
 pub trait Interval {
+    /**
+     * 数据两个区间之间的关系：`包含`，`被包含`，`左侧`，`右侧`，`左侧部分交集`和`右侧部分交集`.
+     */
     fn relation(&self, another: &Self) -> Relation;
 }
