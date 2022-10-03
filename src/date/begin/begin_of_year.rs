4d1430c2a651e2f7e19474a5a3dab02949ca6ffb
silence_zhpf <silence_zhpf@aliyun.com>
2022-10-03 22:09:17 +0800

    feat: ✨ 添加begin_of_year方法

---
+++ src/date/begin/begin_of_year.rs
@@ -0,0 +1,65 @@
+use chrono::{Date, DateTime, Datelike, TimeZone};
+#[cfg(test)]
+mod test {
+    use super::*;
+    use chrono::Utc;
+
+    #[test]
+    fn begin_of_year_test() {
+        let date = Utc.ymd(2008, 8, 8);
+        let actual = Utc.ymd(2008, 1, 1);
+        let result = begin_of_year(date);
+        assert_eq!(Some(actual), result);
+    }
+
+    #[test]
+    fn begin_of_year_with_time_test() {
+        let datetime = Utc.ymd(2008, 8, 8).and_hms(8, 8, 8);
+        let actual = Utc.ymd(2008, 1, 1);
+        let result = begin_of_year_with_time(datetime);
+        assert_eq!(Some(actual), result);
+    }
+}
+/// Get the start of a year.
+/// 获取某年的开始日期
+/// # Arguments
+///
+/// * `date`: 日期
+///
+/// returns: Option<Date<Tz>>
+///
+/// # Examples
+///
+/// ```
+/// use chrono::{TimeZone, Utc};
+/// use hutools::date::begin_of_year;
+/// let date = Utc.ymd(2008,8,8);
+/// let actual = Utc.ymd(2008,1,1);
+/// let result = begin_of_year(date);
+/// assert_eq!(Some(actual), result);
+/// ```
+pub fn begin_of_year<Tz: TimeZone>(date: Date<Tz>) -> Option<Date<Tz>> {
+    date.with_month(1)?.with_day(1)
+}
+
+/// Get the start date of a year.
+/// 获取某年的开始日期
+/// # Arguments
+///
+/// * `datetime`:
+///
+/// returns: Option<Date<Tz>>
+///
+/// # Examples
+///
+/// ```
+/// use chrono::{TimeZone, Utc};
+/// use hutools::date::begin_of_year_with_time;
+/// let datetime = Utc.ymd(2008,8,8).and_hms(8,8,8);
+/// let actual = Utc.ymd(2008,1,1);
+/// let result = begin_of_year_with_time(datetime);
+/// assert_eq!(Some(actual),result);
+/// ```
+pub fn begin_of_year_with_time<Tz: TimeZone>(datetime: DateTime<Tz>) -> Option<Date<Tz>> {
+    datetime.date().with_month(1)?.with_day(1)
+}
--- src/date/begin/mod.rs
+++ src/date/begin/mod.rs
@@ -3,9 +3,11 @@ mod begin_of_hour;
 mod begin_of_minute;
 mod begin_of_month;
 mod begin_of_quarter;
+mod begin_of_year;
 
 pub use begin_of_day::{begin_of_day, begin_of_day_with_time};
 pub use begin_of_hour::begin_of_hour;
 pub use begin_of_minute::begin_of_minute;
 pub use begin_of_month::{begin_of_month, begin_of_month_with_time};
 pub use begin_of_quarter::{begin_of_quarter, begin_of_quarter_with_time};
+pub use begin_of_year::{begin_of_year, begin_of_year_with_time};
