f01c2b1eec5e53a81cd5dddefe53a788af1c05cb
silence_zhpf <silence_zhpf@aliyun.com>
2022-10-06 06:39:43 +0800

    feat: ✨️ 添加between_years方法

---
+++ src/date/between/between_years.rs
@@ -0,0 +1,48 @@
+use chrono::{Date, DateTime, TimeZone};
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use chrono::{Duration, TimeZone, Utc};
+
+    #[test]
+    fn test_between_years_with_time() {
+        let start = Utc.ymd(2008, 8, 8).and_hms(8, 8, 8);
+        let end = Utc.ymd(2018, 8, 8).and_hms(0, 0, 0);
+        let date_range = (start, end);
+        let actual = 9;
+        let result = between_years_with_time(date_range);
+        assert_eq!(result, Some(actual));
+    }
+
+    #[test]
+    fn test_between_years() {
+        let start = Utc.ymd(2008, 8, 8);
+        let end = Utc.ymd(2018, 8, 8);
+        let date_range = (start, end);
+        let actual = 10;
+        let result = between_years(date_range);
+        assert_eq!(result, Some(actual));
+    }
+
+    #[test]
+    fn test_between_years_none() {
+        let end = Utc.ymd(2008, 8, 8);
+        let start = Utc.ymd(2018, 8, 8);
+        let date_range = (start, end);
+        let result = between_years(date_range);
+        assert_eq!(result, None);
+    }
+}
+
+pub fn between_years<Tz: TimeZone>(date_range: (Date<Tz>, Date<Tz>)) -> Option<u32> {
+    let (start, end) = date_range;
+    end.years_since(start)
+}
+
+pub fn between_years_with_time<Tz: TimeZone>(
+    date_range: (DateTime<Tz>, DateTime<Tz>),
+) -> Option<u32> {
+    let (start, end) = date_range;
+    end.years_since(start)
+}
--- src/date/between/mod.rs
+++ src/date/between/mod.rs
@@ -3,9 +3,11 @@ mod between_hours;
 mod between_minutes;
 mod between_seconds;
 mod between_weeks;
+mod between_years;
 
 pub use between_days::between_days;
 pub use between_hours::between_hours;
 pub use between_minutes::between_minutes;
 pub use between_seconds::between_seconds;
 pub use between_weeks::between_weeks;
+pub use between_years::{between_years, between_years_with_time};
