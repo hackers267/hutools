92dda0d7d81bab1316fe24230d3a0afbbc60c849
silence_zhpf <silence_zhpf@aliyun.com>
2022-11-11 11:42:51 +0800

    chore: 🚧 添加部分数字换中文的实现

--- src/convert/mod.rs
+++ src/convert/mod.rs
@@ -1,1 +1,3 @@
 mod number_chinese;
+
+pub use number_chinese::chinese_number;
--- src/convert/number_chinese.rs
+++ src/convert/number_chinese.rs
@@ -19,34 +19,67 @@ mod tests {
     fn to_chinese_number_with_many_zero() {
         let number = 100001;
         let result = chinese_number(number);
-        assert_eq!(result, "十万零一");
+        assert_eq!(result, "一十万零一");
+    }
+
+    #[test]
+    fn section_to_chinese_test() {
+        let number = 123;
+        let result = section_to_chinese(number);
+        assert_eq!(result, "一百二十三");
+    }
+
+    #[test]
+    fn section_to_chinese_two_thousand() {
+        let number = 2000;
+        let result = section_to_chinese(number);
+        assert_eq!(result, "二千")
     }
 }
 
-fn chinese_number(number: i32) -> String {
-    let list = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
-    let utils = [
-        "", "十", "百", "千", "万", "十", "百", "千", "亿", "十", "百", "千", "万", "十", "百",
-        "千",
-    ];
-    let s = reverse(&number.to_string());
-    let mut s = s
-        .chars()
-        .map(|x| x.to_string().parse::<usize>().unwrap())
-        .map(|x| list.get(x).unwrap())
-        .zip(utils.iter())
-        .map(|(&a, &b)| format!("{a}{b}"))
-        .collect::<Vec<String>>();
-    s.reverse();
-    let s = s.join("");
-    println!("{:?}", s);
-    s
+pub fn chinese_number(mut number: u32) -> String {
+    let section_unit_char = ["", "万", "亿", "万亿"];
+    let mut unit_pos = 0;
+    let mut need_zero = false;
+    let mut result = String::new();
+    while number > 0 {
+        let section = number % 10000;
+        let s = section_to_chinese(section);
+        result = if need_zero {
+            format!("{s}{}{}{result}", section_unit_char[unit_pos], "零")
+        } else {
+            format!("{s}{}{result}", section_unit_char[unit_pos])
+        };
+        need_zero = is_need_zone(section);
+        unit_pos += 1;
+        number /= 10000;
+    }
+    return result;
+
+    fn is_need_zone(number: u32) -> bool {
+        number > 0 && number < 1000
+    }
 }
 
-fn reverse(input: &str) -> String {
+fn section_to_chinese(mut number: u32) -> String {
+    let list = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
+    let unit_char = ["", "十", "百", "千"];
+    let mut unit_pos = 0;
     let mut result = String::new();
-    for c in input.chars().rev() {
-        result.push(c);
+    let mut need_zone = true;
+    while number > 0 {
+        let pos = (number % 10) as usize;
+        if pos == 0 {
+            if number == 0 || !need_zone {
+                need_zone = true;
+                result = format!("{}{result}", list[0]);
+            }
+        } else {
+            need_zone = false;
+            result = format!("{}{}{result}", list[pos], unit_char[unit_pos]);
+        }
+        unit_pos += 1;
+        number /= 10;
     }
     result
 }
